678addc2f447a8c29e8780759766a718f730a9e3
Parth Patel <68620884+parth259@users.noreply.github.com>
2021-02-22 15:35:45 -0400

    Created login page

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { StatusBar } from "react-native";
 import { StyleSheet, Text, View } from "react-native";
+import { Button, Input, Image } from "react-native-elements";
 
-export default function LoginScreen() {
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = () => {};
+
   return (
     <View>
-      <Text>Login Screen</Text>
+      <StatusBar style="light" />
+      <Image
+        source={{
+          uri:
+            "https://upload.wikimedia.org/wikipedia/commons/8/8d/Signal-Logo.svg",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Email"
+          autofocus
+          type="email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          type="password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+      <Button containerStyle={styles.button} onPress={signIn} title="Login" />
+      <Button containerStyle={styles.button} type="outline" title="Register" />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({});
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {},
+  button: {},
+});
