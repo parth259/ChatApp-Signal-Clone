2247991ffb3e9c55c2832b1ad3593fdfbb108f85
Parth Patel <68620884+parth259@users.noreply.github.com>
2021-02-22 14:11:02 -0400

    Created login screen and header for that

--- App.js
+++ App.js
@@ -3,15 +3,28 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import LoginScreen from "./screens/LoginScreen";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+const globalScreenOptions = {
+  headerStyle: { backgroundColor: "#2C6BED" },
+  headerTitleStyle: { color: "white" },
+  headerTintColor: "white",
+};
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <h1>Hello Fellas</h1>
-        <h3>I am Parth Patel</h3>
-        <StatusBar style="auto" />
-      </View>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen
+          options={{
+            title: "Lets Sign Up to Signal",
+          }}
+          name="Login"
+          component={LoginScreen}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
---
+++ screens/LoginScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function LoginScreen() {
+  return (
+    <View>
+      <Text>Login Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
