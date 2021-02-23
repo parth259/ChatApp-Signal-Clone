06c1d305c28a4c106d9adbeb61a03e43728a4eba
Parth Patel <68620884+parth259@users.noreply.github.com>
2021-02-23 12:26:36 -0400

    Configured firebase

--- firebase.js
+++ firebase.js
@@ -1,4 +1,6 @@
-
+import * as firebase from "firebase";
+import "firebase/firestore";
+import "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAGTn-UxajD9BloeVD5EdHp_sJDCmNRsDQ",
@@ -9,3 +11,15 @@ const firebaseConfig = {
   appId: "1:324978239490:web:5d8a5b43bb491c0af19f1b",
 };
 
+let app;
+
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app();
+}
+
+const db = app.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
